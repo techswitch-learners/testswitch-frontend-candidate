d0cb925cb75c173a1eb2c0806abd9fd71289f9ef
espe-on <eljoe0@googlemail.com>
2020-04-14 16:03:59 +0100

    installed additional types and removed ts-ignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from 'next';
-// @ts-ignore
 import scss from '../page-styles/index.scss'
 
 const Home: NextPage = () => <h1 className={scss.example}>Hello world! This is the Candidate Frontend</h1>;
