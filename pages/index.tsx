125e4d6667e38f7b1bd4df7e0d5134cc1af77515
espe-on <eljoe0@googlemail.com>
2020-04-14 15:43:02 +0100

    fixing typescript error with ts-ignore and coming back to later

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
-import css from '../page-styles/index.scss'
+// @ts-ignore
+import scss from '../page-styles/index.scss'
 
-const Home: NextPage = () => <h1 className={css.example}>Hello world! This is the Candidate Frontend</h1>;
+const Home: NextPage = () => <h1 className={scss.example}>Hello world! This is the Candidate Frontend</h1>;
 
 export default Home;
