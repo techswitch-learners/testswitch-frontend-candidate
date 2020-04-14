c3da23627b0902054f03e82020e3f2847b11b6c9
espe-on <eljoe0@googlemail.com>
2020-04-14 16:18:27 +0100

    index.scss is now index.module.scss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import scss from '../page-styles/index.scss'
+import scss from '../page-styles/index.module.scss'
 
 const Home: NextPage = () => <h1 className={scss.example}>Hello world! This is the Candidate Frontend</h1>;
 
