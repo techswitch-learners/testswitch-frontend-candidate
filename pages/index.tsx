89145e5f769eb2b55092b9120340875abffa5089
Wendy <alteredco@gmail.com>
2020-04-15 21:07:29 +0100

    add eslint to dev dependencies, eslintrc.json config, eslintignore config, lint:fix in scripts to package.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,5 @@ import { NextPage } from 'next';
 import scss from '../page-styles/index.module.scss'
 
 const Home: NextPage = () => <h1 className={scss.example}>Hello world! This is the Candidate Frontend</h1>;
-
+    
 export default Home;
