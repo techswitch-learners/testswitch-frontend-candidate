31c1497624e461c72b3a6a4870b2907f6bc7caf9
espe-on <eljoe0@googlemail.com>
2020-04-15 16:31:34 +0100

    Jest now runs index.test.tsx properly

---
+++ _test_/index.test.tsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import {render} from '@testing-library/react';
+import Home from "../pages";
+
+test('Check that the home page renders',()=>{
+    let home=render(<Home/>);
+    const tag=home.getByTestId('page-render-test');
+    expect(tag).toBeInTheDocument();
+});
