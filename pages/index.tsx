d6daaf94ccde897c886cfe3c2e142f7b3d6a422d
espe-on <eljoe0@googlemail.com>
2020-04-16 14:24:18 +0100

    Created page for texteditor to live on

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {NextPage} from 'next';
 import scss from '../pageStyles/index.module.scss'
-import TextEditorContainer from "../components/TextEditorContainer";
+import Link from "next/link";
 
 const Home: NextPage = () => 
     <div>
         <h1 className={scss.example} data-testid="page-render-test">Hello world! This is the Candidate Frontend</h1>
-        <TextEditorContainer/>
+        <Link href="/texteditor"><a>Text Editor</a></Link>
     </div>;
 
 export default Home;
