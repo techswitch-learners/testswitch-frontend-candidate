f2f7dcefdace5068b6ce68e6d0e7bf970b466a81
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-16 17:01:14 +0100

    Assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { NextPage } from 'next';
 import scss from '../pageStyles/index.module.scss'
 
-const Home: NextPage = () => <h1 className={scss.example} data-testid="page-render-test">Hello world! This is the Candidate Frontend</h1>;
+const Home: NextPage = () => <h1 className={scss.example} data-testid="page-render-test">Welcome to the Test Zone</h1>;
 
 export default Home;
