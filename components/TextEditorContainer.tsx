6b51d3bf3a71303931f1345cca84fcbbfa3bf59a
espe-on <eljoe0@googlemail.com>
2020-04-16 13:59:15 +0100

    Basic install of Monaco Text Editor

---
+++ components/TextEditorContainer.tsx
@@ -0,0 +1,35 @@
+﻿import React, {useRef, useState} from "react";
+import Editor from '@monaco-editor/react';
+
+function TextEditorContainer() {
+    const [isEditorReady, setIsEditorReady] = useState(false);
+    const valueGetter = useRef();
+
+    // @ts-ignore
+    function handleEditorDidMount(_valueGetter) {
+        setIsEditorReady(true);
+        valueGetter.current = _valueGetter;
+    }
+
+    function handleShowValue() {
+
+        // @ts-ignore
+        alert(valueGetter.current());
+    }
+
+    return (
+        <div>
+            <button onClick={handleShowValue} disabled={!isEditorReady}>
+                Show value
+            </button>
+
+            <Editor
+                height="90vh"
+                language="javascript"
+                value={"// write your code here"}
+                editorDidMount={handleEditorDidMount}
+            /></div>
+    )
+}
+
+export default TextEditorContainer;
