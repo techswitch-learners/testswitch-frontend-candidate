03da3936ce84fa4140b7234a73c6a38e98af0a11
espe-on <eljoe0@googlemail.com>
2020-04-16 13:30:23 +0100

    Example import

---
+++ components/CodeMirrorContainer.tsx
@@ -0,0 +1,9 @@
+﻿import React, {FunctionComponent} from "react";
+import {UnControlled as CodeMirror} from 'react-codemirror2'
+
+const CodeMirrorContainer: FunctionComponent = () => <div><CodeMirror
+    value='<h1>I ♥ react-codemirror2</h1>'
+    onChange={(editor, data, value) => {
+    }}/></div>;
+
+export default CodeMirrorContainer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { NextPage } from 'next';
+import {NextPage} from 'next';
 import scss from '../pageStyles/index.module.scss'
+import CodeMirrorContainer from "../components/CodeMirrorContainer";
 
-const Home: NextPage = () => <h1 className={scss.example} data-testid="page-render-test">Hello world! This is the Candidate Frontend</h1>;
+const Home: NextPage = () => 
+    <div>
+        <h1 className={scss.example} data-testid="page-render-test">Hello world! This is the Candidate Frontend</h1>
+        <CodeMirrorContainer/>
+    </div>;
 
 export default Home;
