470e5abfa09e073ca61cde905eca7e58a557374c
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-17 16:23:31 +0100

    Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { NextPage } from 'next';
 import scss from '../pageStyles/index.module.scss'
 import Layout from "../components/Layout/layout";
+import Test from "./test";
+import Link from "next/link";
 
 const Home: NextPage = () => {
     return (
         <Layout>
         <section className={scss.content}>
             <h1 className={scss.welcome}>Welcome to the Test Zone</h1>
+            <a className={scss.buttonYellow}>Start Test</a>
         </section>
         </Layout>
     )
---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import { NextPage } from 'next';
+import Layout from "../components/Layout/layout";
+import scss from "../pageStyles/index.module.scss";
+
+const Test: NextPage = () => {
+    return (
+        <Layout>
+            <section className={scss.content}> </section>
+        </Layout>
+    )
+};
+export default Test;
