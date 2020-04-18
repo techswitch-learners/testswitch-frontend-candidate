7fd0a95b43a825cba598bf4bff08ec2e605f5baa
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-18 16:06:35 +0100

    Test bug fixed

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,9 +1,9 @@
-﻿﻿import React from 'react';
-import {render} from '@testing-library/react';
-import Home from "../pages";
+﻿import React from 'react'
+import {render} from '@testing-library/react'
+import Home from "../pages"
 
 test('Check that the home page renders',()=>{
-    let home=render(<Home/>);
+    const home=render(<Home/>);
     const tag=home.getByTestId('page-render-test');
     expect(tag).toBeInTheDocument();
 });
