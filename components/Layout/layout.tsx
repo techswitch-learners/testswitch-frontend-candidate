d24c7141156296c3a074e548bb01ebb997530360
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-18 15:35:36 +0100

    Lint passing

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import LogoHeader from "./logoHeader";
 import scss from '../../pageStyles/layout.module.scss';
 
-const Layout: any = (props: { children: React.ReactNode; }) => (
+const Layout: any = (props: { children: React.ReactNode }) => (
     <div className={scss.layout}>
         <LogoHeader/>
             {props.children}
--- components/Layout/logoHeader.tsx
+++ components/Layout/logoHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import scss from '../../pageStyles/layout.module.scss';
 
-const LogoHeader = () => (
+const LogoHeader: any = () => (
     <img className={scss.logoHeader} alt="TechSwitch" src="https://www.techswitch.co.uk/images/logo.svg">
     </img>
 );
