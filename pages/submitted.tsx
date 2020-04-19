06ef454e43b3abf9013f76acf89712e1b88ea702
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-19 19:33:07 +0100

    Submitted success page

---
+++ pages/submitted.tsx
@@ -0,0 +1,19 @@
+﻿﻿﻿import { NextPage } from 'next';
+import Layout from "../components/Layout/layout";
+import scss from "../pageStyles/index.module.scss";
+import React from "react";
+
+const Submitted: NextPage = () => {
+    return (
+        <Layout>
+            <section className={scss.content}>
+                <h1 className={scss.welcome}>Your Test Has Been Submitted</h1>
+                <div className={scss.content}>
+                    <h2 className={scss.instructionsHeader}>We will review your results and get back to you by email shortly</h2>
+                </div>
+                <h1 className={scss.welcome}>Thank You!</h1>
+            </section>
+        </Layout>
+    )
+};
+export default Submitted;
