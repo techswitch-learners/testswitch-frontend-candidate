e21814d5233fcabdb747b6e3163fd55add8b78c9
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-19 19:36:34 +0100

    Submitted success page

--- pages/submitted.tsx
+++ pages/submitted.tsx
@@ -1,4 +1,4 @@
-﻿﻿﻿import { NextPage } from 'next';
+﻿import {NextPage} from 'next';
 import Layout from "../components/Layout/layout";
 import scss from "../pageStyles/index.module.scss";
 import React from "react";
@@ -8,12 +8,12 @@ const Submitted: NextPage = () => {
         <Layout>
             <section className={scss.content}>
                 <h1 className={scss.welcome}>Your Test Has Been Submitted</h1>
-                <div className={scss.content}>
-                    <h2 className={scss.instructionsHeader}>We will review your results and get back to you by email shortly</h2>
-                </div>
+                    <div className={scss.content}>
+                        <h2 className={scss.instructionsHeader}>We will review your results and get back to you by email shortly</h2>
+                    </div>
                 <h1 className={scss.welcome}>Thank You!</h1>
             </section>
         </Layout>
-    )
+)
 };
 export default Submitted;
