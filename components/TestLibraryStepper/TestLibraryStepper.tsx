56678f4e9a4c2d105794bdfd3838d0b162f52b91
Wendy <alteredco@gmail.com>
2020-04-20 23:41:53 +0100

    create progress stepper with material ui

--- components/TestLibraryStepper/TestLibraryStepper.tsx
+++ components/TestLibraryStepper/TestLibraryStepper.tsx
@@ -1,14 +1,63 @@
-﻿import React from "react";
+﻿import React, {useState} from "react";
+import { Stepper, Step, StepLabel, StepConnector, Typography, Button} from '@material-ui/core';
+import {Check, MoreHoriz} from '@material-ui/icons';
 
-function TestLibraryStepper() {
-    return (
-        <div className="stepper-container">
-            <h2>Test 1</h2>
-            <div className="step"></div>
-            <button></button>
-        </div>
-    );
+function getSteps() {
+    return ['Test 1', 'Test 2', 'Test 3'];
 }
 
-export default TestLibraryStepper;
+function getStepContent(step:number){
+    switch (step) {
+        case 0:
+            return 'Test 1';
+        case 1:
+            return 'Test 2';
+        case 2:
+            return 'Test 3';
+    }
+}
+
+export default function TestLibraryStepper(){
+    const [activeStep, setActiveStep] = useState(1);
+    const steps = getSteps();
+    
+    const getNext = () => {
+        setActiveStep((prevActiveStep)=> prevActiveStep+1);
+    };
+    
+    return (
+        <div className="stepper-container">
+            <Stepper alternativeLabel activeStep={activeStep}>
+                {steps.map((label)=>(
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+            <div>
+                {activeStep === steps.length ? (
+                    <div>
+                        <Typography className="finished">
+                            All steps completed - you&apos;re finished
+                        </Typography>
+                    </div>
+                ) : (
+                    <div>
+                        <Typography>{getStepContent(activeStep)}</Typography>
+                        <div>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={getNext}
+                            >
+                                {activeStep === steps.length - 1 ? 'Start Final Test' : 'Start Next Test'}
+                            </Button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+
+};
     
--- pages/testlibrary.tsx
+++ pages/testlibrary.tsx
@@ -1,13 +1,15 @@
 ﻿import React from "react";
-import { NextPage} from "next";
+import {NextPage} from "next";
 import scss from '../pageStyles/index.module.scss'
 import Link from "next/link";
 import TestLibraryStepper from "../components/TestLibraryStepper/TestLibraryStepper";
+import Layout from "../components/Layout/layout";
 
-const TestLibrary : NextPage = () => 
-    <div>
-        <h1>Welcome to the Test Library</h1>
-        <TestLibraryStepper/>
-    </div>;
-    
-    export default TestLibrary;
+const TestLibrary: NextPage = () =>
+    <Layout>
+        <div>
+            <TestLibraryStepper/>
+        </div>
+    </Layout>;
+
+export default TestLibrary;
