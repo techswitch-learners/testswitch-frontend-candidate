2f940ca2a7d0dabc17ffc6fb1351f7c82269da98
espe-on <eljoe0@googlemail.com>
2020-04-21 14:03:16 +0100

    Created three editors on texteditor page which highlight the different styles of autocomplete and highlighting.

--- components/TextEditor/Settings/FullAutoComplete.ts
+++ components/TextEditor/Settings/FullAutoComplete.ts
@@ -1,6 +1,40 @@
 ﻿import {TextEditorOptions} from "../../../Models/TextEditorOptions";
 
 const fullAutoComplete: TextEditorOptions = {
-
+    autoClosingBrackets: "languageDefined",
+    autoClosingQuotes : "languageDefined",
+    codeLens: true,
+    lightbulb : {
+        enabled : true
+    },
+    folding : true,
+    suggest: {
+        showMethods: true,
+        showFunctions: true,
+        showConstructors: true,
+        showFields: true,
+        showVariables: true,
+        showClasses: true,
+        showStructs: true,
+        showInterfaces: true,
+        showModules: true,
+        showProperties: true,
+        showEvents: true,
+        showOperators: true,
+        showUnits: true,
+        showValues: true,
+        showConstants: true,
+        showEnums: true,
+        showEnumMembers: true,
+        showKeywords: true,
+        showWords: true,
+        showColors: true,
+        showFiles: true,
+        showReferences: true,
+        showFolders: true,
+        showTypeParameters: true,
+        showSnippets: true,
+        maxVisibleSuggestions: 5,
+    }
 };
 export default fullAutoComplete;
--- pages/texteditor.tsx
+++ pages/texteditor.tsx
@@ -2,16 +2,37 @@
 import {NextPage} from "next";
 import TextEditorContainer from "../components/TextEditor/TextEditorContainer";
 import noAutoComplete from "../components/TextEditor/Settings/NoAutoComplete";
+import limitedAutoComplete from "../components/TextEditor/Settings/LimitedAutoComplete";
+import fullAutoComplete from "../components/TextEditor/Settings/FullAutoComplete";
 
 const TextEditor: NextPage = () => <div>
-    <h1>This is the Text Editor</h1>
+    <h1>These are the different Text Editors</h1>
+    <h2>A Text Editor with no highlighting or autocomplete</h2>
+    <TextEditorContainer containerClassName="textEditor"
+                         theme="dark"
+                         height="90vh"
+                         width="90vw"
+                         language="unset"
+                         defaultText="//Enter Your Answer Here. no autocomplete"
+                         options={noAutoComplete}
+    />
+    <h2>A Text Editor with limited highlighting & autocomplete</h2>
     <TextEditorContainer containerClassName="textEditor"
                          theme="dark"
                          height="90vh"
                          width="90vw"
                          language="javascript"
-                         defaultText="//Enter Your Answer Here."
-                         options={noAutoComplete}
+                         defaultText="//Enter Your Answer Here. limited autocomplete"
+                         options={limitedAutoComplete}
+    />
+    <h2>A Text Editor with full highlighting & autocomplete</h2>
+    <TextEditorContainer containerClassName="textEditor"
+                         theme="dark"
+                         height="90vh"
+                         width="90vw"
+                         language="javascript"
+                         defaultText="//Enter Your Answer Here. full autocomplete"
+                         options={fullAutoComplete}
     />
 </div>;
 
