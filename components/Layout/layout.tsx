4eee24a17d7d404bf217ed0cf3fda4957b3cea95
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-21 13:37:36 +0100

    Accessibility improvements made: Head added to HTML including title and language

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import scss from './layout.module.scss';
+import Head from "next/head";
 
 type LayoutProps = {children: React.ReactNode };
 const Layout = ({children}: LayoutProps): JSX.Element => (
-    <div className={scss.layout}>
-        <img className={scss.logoHeader} alt="TechSwitch" src='/logo.png'>
+    <html title={"layout"} lang={"en"} className={scss.layout}>
+    <Head>
+        <title>TestSwitch</title>
+        <meta name="layout" content="initial-scale=1.0, width=device-width" />
+    </Head>    
+    <img className={scss.logoHeader} alt="TechSwitch" src='/logo.png'>
         </img>
         {children}
-    </div>
+    </html>
 );
 
 export default Layout;
