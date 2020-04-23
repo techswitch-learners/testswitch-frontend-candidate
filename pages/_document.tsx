7853488a8a72eb41c6e70db5e003db671d413441
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-23 12:27:10 +0100

    Clean

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, {Html, Head, Main, NextScript, DocumentInitialProps} from 'next/document'
+import React from "react";
+import {NextPageContext} from "next";
+import {RenderPage} from "next/dist/next-server/lib/utils";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: NextPageContext & { renderPage: RenderPage }): Promise<DocumentInitialProps> {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html lang={"en"} title={"TestSwitch"}>
+                <Head><title>TestSwitch</title></Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
