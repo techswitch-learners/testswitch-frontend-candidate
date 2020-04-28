c76f82d070c570db1c69e3d561765b660c2f0129
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-28 16:09:30 +0100

    removed default time and js

--- components/CandidateTestView/Tests/AddTwoIntegers.tsx
+++ components/CandidateTestView/Tests/AddTwoIntegers.tsx
@@ -1,4 +1,4 @@
-import {CandidateTestModel, DefaultExecutionTimeLimit, DefaultJsTips} from "../../../Models/CandidateTestModel";
+import {CandidateTestModel} from "../../../Models/CandidateTestModel";
 
 export const AddTwoIntegersTest: CandidateTestModel = {
     number: 1,
@@ -7,10 +7,9 @@ export const AddTwoIntegersTest: CandidateTestModel = {
     input: 29,
     brief: "You are given a two-digit integer n. Return the sum of its digits.",
     example: "For n = 29, the output should be\n" + "addTwoDigits(n) = 11.",
-    executionTimeLimit: DefaultExecutionTimeLimit,
     inputType: "integer n",
     inputDescription: "A positive two-digit integer.\n" + "Guaranteed constraints:\n" + "10 ≤ n ≤ 99.",
     outputType: "integer",
     outputDescription: "The sum of the first and second digits of the input number.",
-    testStarterCode: "function addTwoDigits(n) {\n}",
-    jsTips: DefaultJsTips};
+    testStarterCode: "function addTwoDigits(n) {\n}"
+};
