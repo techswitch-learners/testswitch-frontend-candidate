8cdef9e1ede1c3429611a48e0b7ae3fad56f469e
Wendy <alteredco@gmail.com>
2020-04-28 17:17:27 +0100

    change background to black, text to white and add accessibility landmark to layout.tsx

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -3,11 +3,11 @@ import scss from './layout.module.scss';
 
 type LayoutProps = {children: React.ReactNode };
 const Layout = ({children}: LayoutProps): JSX.Element => (
-    <div className={scss.layout}>
+    <main className={scss.layout}>
         <img className={scss.logoHeader} alt="TechSwitch" src='/logo.png'>
         </img>
         {children}
-    </div>
+    </main>
 );
 
 export default Layout;
--- components/TestLibraryStepper/TestLibraryStepper.tsx
+++ components/TestLibraryStepper/TestLibraryStepper.tsx
@@ -3,7 +3,7 @@ import {Step, StepLabel, Stepper, Typography} from '@material-ui/core';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import {CandidateTestStatus} from "../../pages/api/candidateApiClient.module";
 import TestSwitchStepIcon from "../TestLibraryStepperIcons/TestLibraryStepperIcons";
-import {TestSwitchConnector, TestSwitchTheme} from "../TestLibraryOverrides/TestLibraryOverrides"
+import {h1Style, TestSwitchConnector, TestSwitchTheme} from "../TestLibraryOverrides/TestLibraryOverrides"
 import Link from "next/link";
 
 interface TestLibraryStepperProps {
@@ -27,14 +27,15 @@ export default function TestLibraryStepper(props: TestLibraryStepperProps): JSX.
     //TODO this is example data, set active step with candidate number of results, maybe props.candidateTestStatus.results.length?
     const activeStep = 1;
 
-
     return (
         <article className="stepperContainer">
             <MuiThemeProvider theme={TestSwitchTheme}>
-                <Stepper alternativeLabel activeStep={activeStep} connector={<TestSwitchConnector/>}>
+                <Stepper style={{backgroundColor: "transparent"}} alternativeLabel activeStep={activeStep}
+                         connector={<TestSwitchConnector/>}>
                     {steps.map((label, status) => (
                         <Step key={label}>
-                            <StepLabel StepIconComponent={TestSwitchStepIcon} className="stepLabel"><h1>{label}</h1>
+                            <StepLabel StepIconComponent={TestSwitchStepIcon} className="stepLabel"><h1
+                                style={h1Style}>{label}</h1>
                             </StepLabel>
                         </Step>
                     ))}
@@ -42,17 +43,17 @@ export default function TestLibraryStepper(props: TestLibraryStepperProps): JSX.
             </MuiThemeProvider>
             <section className="stepperBtnContainer">
                 {activeStep === steps.length ? (
-                        <Typography align={"center"} className="finished">
-                            All tests completed.
-                        </Typography>
+                    <Typography align={"center"} className="finished">
+                        All tests completed.
+                    </Typography>
                 ) : (
-                        <Typography align={"center"}>
-                            <Link href={`/test${activeStep + 1}`}>
-                                <a className="stepperBtn">
-                                    {activeStep === steps.length - 1 ? 'Start Final Test' : `Start Test ${activeStep + 1}`}
-                                </a>
-                            </Link>
-                        </Typography>
+                    <Typography align={"center"}>
+                        <Link href={`/test${activeStep + 1}`}>
+                            <a className="stepperBtn">
+                                {activeStep === steps.length - 1 ? 'Start Final Test' : `Start Test ${activeStep + 1}`}
+                            </a>
+                        </Link>
+                    </Typography>
                 )}
             </section>
         </article>
