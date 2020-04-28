83fd25ef46d280bb710b38b7ff809afdccf63f8c
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-28 15:17:48 +0100

    Input/Output removed from render

--- components/CandidateTestView/Instructions/Instructions.tsx
+++ components/CandidateTestView/Instructions/Instructions.tsx
@@ -12,11 +12,6 @@ const InstructionsBlock = (testModel: CandidateTestModel): JSX.Element => (
             <li className={scss.instructionList}><strong>[input] {testModel.inputType}</strong><br/>{testModel.inputDescription}</li>
             <li className={scss.instructionList}><strong>[output] {testModel.outputType}</strong><br/>{testModel.outputDescription}</li>
         </ul>
-        <section className={scss.sampleInputOutput}>
-        <h2>TEST {testModel.number}</h2>
-        <p className={scss.instructionList}><strong>Input:</strong> {testModel.input}</p>
-        <p className={scss.instructionList}><strong>Expected Output:</strong> {testModel.expectedOutput}</p>
-        </section>
     </div>
 );
 
