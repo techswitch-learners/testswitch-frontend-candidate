74147b269d2fee64d6e568bd92e8c36d8611cd69
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-28 12:15:37 +0100

    Resolving text editor conflicts

--- components/CandidateTestView/CandidateTestView.tsx
+++ components/CandidateTestView/CandidateTestView.tsx
@@ -4,21 +4,24 @@ import {CandidateTestModel} from "../../Models/CandidateTestModel";
 import TextEditorContainer from "../TextEditor/TextEditorContainer";
 import InstructionsBlock from "./Instructions/Instructions";
 
-const CandidateTestView = (testModel: CandidateTestModel): JSX.Element => (
+interface CandidateTestViewProps {
+    test: CandidateTestModel;
+}
+const CandidateTestView = (candidateTestViewProps: CandidateTestViewProps): JSX.Element => (
     <div>
-        <h1 className={scss.testTitle}>Test {testModel.number}: {testModel.title}</h1>
+        <h1 className={scss.testTitle}>Test {candidateTestViewProps.test.number}: {candidateTestViewProps.test.title}</h1>
         <div className={scss.testPage}>
             <section className={scss.testInstructions}>
-                <h2 className={scss.instructionsHeader}>{testModel.title}</h2>
+                <h2 className={scss.instructionsHeader}>{candidateTestViewProps.test.title}</h2>
                 <div id={"testInstructions"} className={scss.instructions}>
-                    {InstructionsBlock(testModel)}
+                    {InstructionsBlock(candidateTestViewProps.test)}
                 </div>
             </section>
             <section className={scss.textEditor}>
                 <TextEditorContainer 
                                      height="50vh" 
                                      width="50vw"
-                                     defaultText={testModel.testStarterCode}
+                                     defaultText={candidateTestViewProps.test.testStarterCode}
                 />
             </section>
         </div>
--- pages/testpage.tsx
+++ pages/testpage.tsx
@@ -8,7 +8,7 @@ const TestPage: NextPage = () => {
     const testToRender = TestList[1];
     return (
         <Layout>
-            <CandidateTestView number={testToRender.number} title={testToRender.title} brief={testToRender.brief} example={testToRender.example} executionTimeLimit={testToRender.executionTimeLimit} jsTips={testToRender.jsTips} expectedOutput={testToRender.expectedOutput} input={testToRender.input} inputDescription={testToRender.inputDescription} inputType={testToRender.inputType} testStarterCode={testToRender.testStarterCode} outputDescription={testToRender.outputDescription} outputType={testToRender.outputType}/>
+            <CandidateTestView test={testToRender}/>
         </Layout>
     )
 };
