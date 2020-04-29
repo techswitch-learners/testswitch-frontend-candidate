47c2e4cb1342930601cab16dca758dff18bd391c
Beatrice <beatrice.opara@softwire.com>
2020-04-29 11:54:08 +0100

    created query token component

---
+++ components/QueryToken/QueryToken.tsx
@@ -0,0 +1,17 @@
+﻿import React from "react";
+import {useRouter} from "next/router";
+import Link from "next/link";
+
+
+type QueryTokenProps = {children: React.ReactNode, href: string}
+
+const QueryToken = ({children, href}:QueryTokenProps) => {
+  const router = useRouter();
+  return (
+      <Link href={`${href}?token=${router.query.guid}`}>
+          {children}
+      </Link>
+  )
+};
+
+export default QueryToken;
