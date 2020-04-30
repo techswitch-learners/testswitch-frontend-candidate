d5d0a6925c9b32cf15c142d42cd46af803abe5b9
Beatrice <beatrice.opara@softwire.com>
2020-04-30 15:21:37 +0100

    replaced link tags with query token component

--- components/QueryToken/QueryToken.tsx
+++ components/QueryToken/QueryToken.tsx
@@ -7,11 +7,15 @@ type QueryTokenProps = {children: React.ReactNode, href: string}
 
 const QueryToken = ({children, href}:QueryTokenProps) => {
   const router = useRouter();
+  
   return (
-      <Link href={`${href}?token=${router.query.guid}`}>
+      <Link href={`${href}?token=${router.query.token}`}>
           {children}
       </Link>
   )
+    
 };
+
+
 
 export default QueryToken;
