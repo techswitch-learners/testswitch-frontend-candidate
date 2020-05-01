3ad2aa5e496e9c9989beb29b0d87a6f6b2a1e238
Beatrice <beatrice.opara@softwire.com>
2020-05-01 18:32:15 +0100

    wrote test to include mock router
    test currently fails

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,9 +1,20 @@
-﻿import React from 'react';
+﻿import React, {FunctionComponent} from 'react';
 import {render} from '@testing-library/react';
 import Home from "../pages";
+import { RouterContext } from 'next-server/dist/lib/router-context'
 
-test('Check that the home page renders', () => {
-    const home = render(<Home/>);
+
+test('Check that the home page renders', async () => {
+   const router = {
+       query:{token: "undefined"},
+   };
+    const home = render(
+        <RouterContext.Provider value={router.query}>
+            <Home/>
+        </RouterContext.Provider>
+        )
+    ;
     const tag = home.getByTestId('page-render-test');
     expect(tag).toBeInTheDocument();
 });
+
