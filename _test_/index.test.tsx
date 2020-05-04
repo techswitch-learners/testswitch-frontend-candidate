5c7d3ab5555f3be15913c317d907454b303377db
Beatrice <beatrice.opara@softwire.com>
2020-05-04 14:56:53 +0100

    created mock router function for testing
    added to test to ensure page everything on the page works

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,20 +1,29 @@
-﻿import React, {FunctionComponent} from 'react';
+﻿import React from 'react';
 import {render} from '@testing-library/react';
 import Home from "../pages";
 import { RouterContext } from 'next-server/dist/lib/router-context'
-
+import {mockNextUseRouter} from "./mockNextUseRouter";
 
 test('Check that the home page renders', async () => {
-   const router = {
-       query:{token: "undefined"},
-   };
+    
+    const router = mockNextUseRouter({
+        route: "/testpage",
+        pathname: "/testpage",
+        query: "",
+        asPath: `/testpage?token=${encodeURIComponent("100")}`
+    });
+    
+   const Home = require('../pages/testpage').default;
+   
     const home = render(
-        <RouterContext.Provider value={router.query}>
+        
+        <RouterContext.Provider value={Home(router)}>
             <Home/>
         </RouterContext.Provider>
         )
     ;
     const tag = home.getByTestId('page-render-test');
+
     expect(tag).toBeInTheDocument();
 });
 
