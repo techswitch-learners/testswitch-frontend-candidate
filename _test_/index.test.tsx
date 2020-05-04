924aad6440454f54ddb04296abb0e5b616a2b6f8
Beatrice <beatrice.opara@softwire.com>
2020-05-04 19:06:39 +0100

    added @ts-no check to disable type-checking
    disable type checking for index.test file

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,4 +1,5 @@
-﻿import React from 'react';
+﻿// @ts-nocheck
+import React from 'react';
 import {render} from '@testing-library/react';
 import Home from "../pages";
 import * as NextRouter from "next/router";
@@ -6,8 +7,7 @@ import * as NextRouter from "next/router";
 test('Check that the home page renders', async () => {
     
     const mockNextUseRouter = jest.spyOn(NextRouter, "useRouter");
-    //@ts-ignore 
-    //esline-disable-next-line
+    
     mockNextUseRouter.mockReturnValue({query: {token: "100"}});
    
     const home = render(<Home/>);
