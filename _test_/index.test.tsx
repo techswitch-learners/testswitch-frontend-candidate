02032ae0ea9b6343d534048a01f1c0fca9efd31f
Beatrice <beatrice.opara@softwire.com>
2020-05-04 18:01:41 +0100

    created import to fix linter error
    added ts-ignore , do not need all of router objects

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,11 +1,14 @@
 ﻿import React from 'react';
 import {render} from '@testing-library/react';
 import Home from "../pages";
+import * as NextRouter from "next/router";
 
 test('Check that the home page renders', async () => {
-    const mockNextUseRouter = jest.spyOn(require("next/router"), "useRouter");
     
-    mockNextUseRouter.mockReturnValue({query: {token: "100"},});
+    const mockNextUseRouter = jest.spyOn(NextRouter, "useRouter");
+    
+    // @ts-ignore
+    mockNextUseRouter.mockReturnValue({query: {token: "100"}});
    
     const home = render(<Home/>);
     
