358d22f342ae15ee79ceb7ce0e1e3e106bbb3ed9
Beatrice <beatrice.opara@softwire.com>
2020-05-04 17:00:31 +0100

    created mock router in index.test.tsx
    removed mockNextUseRouter file

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,29 +1,15 @@
 ﻿import React from 'react';
 import {render} from '@testing-library/react';
 import Home from "../pages";
-import { RouterContext } from 'next-server/dist/lib/router-context'
-import {mockNextUseRouter} from "./mockNextUseRouter";
 
 test('Check that the home page renders', async () => {
+    const mockNextUseRouter = jest.spyOn(require("next/router"), "useRouter");
     
-    const router = mockNextUseRouter({
-        route: "/testpage",
-        pathname: "/testpage",
-        query: "",
-        asPath: `/testpage?token=${encodeURIComponent("100")}`
-    });
-    
-   const Home = require('../pages/testpage').default;
+    mockNextUseRouter.mockReturnValue({query: {token: "100"},});
    
-    const home = render(
-        
-        <RouterContext.Provider value={Home(router)}>
-            <Home/>
-        </RouterContext.Provider>
-        )
-    ;
+    const home = render(<Home/>);
+    
     const tag = home.getByTestId('page-render-test');
 
     expect(tag).toBeInTheDocument();
 });
-
