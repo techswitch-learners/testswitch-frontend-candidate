d58fd74cf430c0c6905ffcf721fc630ae9c33818
Beatrice <beatrice.opara@softwire.com>
2020-05-05 12:05:55 +0100

    added @ts-ignore for mock router to run in test

--- _test_/index.test.tsx
+++ _test_/index.test.tsx
@@ -1,5 +1,4 @@
-﻿// @ts-nocheck
-import React from 'react';
+﻿import React from 'react';
 import {render} from '@testing-library/react';
 import Home from "../pages";
 import * as NextRouter from "next/router";
@@ -7,7 +6,8 @@ import * as NextRouter from "next/router";
 test('Check that the home page renders', async () => {
     
     const mockNextUseRouter = jest.spyOn(NextRouter, "useRouter");
-    
+    //eslint-disable-next-line
+    // @ts-ignore
     mockNextUseRouter.mockReturnValue({query: {token: "100"}});
    
     const home = render(<Home/>);
