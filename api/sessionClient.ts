e2e8db07e2f88c6c0d438771875e4250c1f47f89
Emmalu Fox <emmalu.fox@softwire.com>
2020-05-05 13:28:59 +0100

    Incorporating tokens

--- api/sessionClient.ts
+++ api/sessionClient.ts
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 export async function checkToken(token: string): Promise<boolean> {
     const {publicRuntimeConfig} = getConfig();
     const baseUrl = publicRuntimeConfig.API_URL;
-    
-    const response = await fetch(`${baseUrl}/sessions/${token}`);
+
+    const response = await fetch(`https://testswitch-api-staging.herokuapp.com/sessions/${token}`);
     return response.ok;
 }
