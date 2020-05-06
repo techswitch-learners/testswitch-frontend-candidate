83e9d9257fd2de73e9b6275c50c659513487af0f
Emmalu Fox <emmalu.fox@softwire.com>
2020-05-06 17:10:53 +0100

    error page

--- pages/404Error.tsx
+++ pages/404Error.tsx
@@ -6,8 +6,8 @@ import React from "react";
 const AccessDenied: NextPage = () => (
     <Layout>
         <section className={scss.content}>
-            <h1 className={scss.submitHeader}>Your Test Has Been Submitted</h1>
-            <p className={scss.submitMessage}>We will review your results and get back to you by email shortly</p>
+            <h1 className={scss.submitHeader}>404 Error: Page Cannot be Found</h1>
+            <p className={scss.submitMessage}>Please use the unique link sent to you by email to access your tests</p>
             <h2 className={scss.submitHeader}>Thank You!</h2>
         </section>
     </Layout>
