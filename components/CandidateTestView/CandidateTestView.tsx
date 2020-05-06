246ec9b645d8e7b383c6c340ae90553028470def
Emmalu Fox <emmalu.fox@softwire.com>
2020-05-06 14:31:13 +0100

    Instructions scss module returned

--- components/CandidateTestView/CandidateTestView.tsx
+++ components/CandidateTestView/CandidateTestView.tsx
@@ -9,18 +9,17 @@ interface CandidateTestViewProps {
     token: string;
 }
 const CandidateTestView = (candidateTestViewProps: CandidateTestViewProps): JSX.Element => (
-    
+
         <div className={scss.testPage}>
             <h1 className={scss.testTitle}>Test {candidateTestViewProps.test.number}: {candidateTestViewProps.test.title}</h1>
             <section className={scss.testInstructions}>
-                <p className={scss.instructionsSubheader}><strong>{candidateTestViewProps.test.title}</strong></p>
                 <div id={"testInstructions"} className={scss.instructions}>
                     {InstructionsBlock(candidateTestViewProps.test)}
                 </div>
             </section>
             <div className={scss.textEditor}>
-                <TextEditorContainer 
-                     height="50vh" 
+                <TextEditorContainer
+                     height="50vh"
                      width="100%"
                      defaultText={candidateTestViewProps.test.testStarterCode}
                      token={candidateTestViewProps.token}
--- components/CandidateTestView/Instructions/Instructions.tsx
+++ components/CandidateTestView/Instructions/Instructions.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import scss from "../Instructions.module.scss";
+import scss from "../Instructions/Instructions.module.scss";
 import {CandidateTestModel} from "../../../Models/CandidateTestModel";
 
 const InstructionsBlock = (testModel: CandidateTestModel): JSX.Element => (
     <div className={scss.testInstructions}>
-        <p className={scss.instructionList}>{testModel.brief}</p>
-        <p className={scss.instructionsSubheader}><strong>Example</strong></p>
-        <p className={scss.instructionList}>{testModel.example}</p>
-        <p className={scss.instructionsSubheader}><strong>Input/Output</strong></p>
-        <ul className={scss.instructionList}>
+        <h1 className={scss.instructionsSubheader}><strong>{testModel.title}</strong></h1>
+        <p className={scss.instructionsBody}>{testModel.brief}</p>
+        <h1 className={scss.instructionsSubheader}><strong>Example</strong></h1>
+        <p className={scss.instructionsBody}>{testModel.example}</p>
+        <h1 className={scss.instructionsSubheader}><strong>Input/Output</strong></h1>
+        <ul className={scss.instructionsBody}>
             <li className={scss.instructionList}><strong>[input] {testModel.inputType}</strong><br/>{testModel.inputDescription}</li>
             <li className={scss.instructionList}><strong>[output] {testModel.outputType}</strong><br/>{testModel.outputDescription}</li>
         </ul>
