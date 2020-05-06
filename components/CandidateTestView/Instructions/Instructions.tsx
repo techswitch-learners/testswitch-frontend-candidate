98d943486a0c4054cfca58435e7cd1cb9cdb3dc6
Emmalu Fox <emmalu.fox@softwire.com>
2020-05-06 16:05:09 +0100

    Replacing vw/vh with px where possible

--- components/CandidateTestView/Instructions/Instructions.tsx
+++ components/CandidateTestView/Instructions/Instructions.tsx
@@ -4,11 +4,11 @@ import {CandidateTestModel} from "../../../Models/CandidateTestModel";
 
 const InstructionsBlock = (testModel: CandidateTestModel): JSX.Element => (
     <div className={scss.testInstructions}>
-        <h1 className={scss.instructionsSubheader}><strong>{testModel.title}</strong></h1>
+        <h2 className={scss.instructionsSubheader}><strong>{testModel.title}</strong></h2>
         <p className={scss.instructionsBody}>{testModel.brief}</p>
-        <h1 className={scss.instructionsSubheader}><strong>Example</strong></h1>
+        <h2 className={scss.instructionsSubheader}><strong>Example</strong></h2>
         <p className={scss.instructionsBody}>{testModel.example}</p>
-        <h1 className={scss.instructionsSubheader}><strong>Input/Output</strong></h1>
+        <h2 className={scss.instructionsSubheader}><strong>Input/Output</strong></h2>
         <ul className={scss.instructionsBody}>
             <li className={scss.instructionList}><strong>[input] {testModel.inputType}</strong><br/>{testModel.inputDescription}</li>
             <li className={scss.instructionList}><strong>[output] {testModel.outputType}</strong><br/>{testModel.outputDescription}</li>
