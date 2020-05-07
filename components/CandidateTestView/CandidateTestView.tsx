0b3db5bca6a73b932d1bd14f4b99debd62c39fd3
Emmalu Fox <emmalu.fox@softwire.com>
2020-05-07 10:40:12 +0100

    latest

--- components/CandidateTestView/CandidateTestView.tsx
+++ components/CandidateTestView/CandidateTestView.tsx
@@ -9,7 +9,6 @@ interface CandidateTestViewProps {
     token: string;
 }
 const CandidateTestView = (candidateTestViewProps: CandidateTestViewProps): JSX.Element => (
-
         <div className={scss.testPage}>
             <h1 className={scss.testTitle}>Test {candidateTestViewProps.test.number}: {candidateTestViewProps.test.title}</h1>
             <section className={scss.testInstructions}>
